767c1dc23711a50ad673754fc183d66556dd49e2
Mihucza, Bence <mihben@gmail.com>
2025-03-20 08:14:02 +0100

    chore: Integrated Lightinject

--- src/SpecStore/Program.cs
+++ src/SpecStore/Program.cs
@@ -1,8 +1,16 @@
+using LightInject;
+using SpecStore.Wireup;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Host.UseLightInject();
+
+Wireup.ConfigureServices(builder.Services, builder.Configuration);
+builder.Host.ConfigureContainer<IServiceRegistry>(Wireup.ConfigureContainer);
 
 var app = builder.Build();
 
---
+++ src/SpecStore/Wireup/Wireup.cs
@@ -0,0 +1,17 @@
+﻿using LightInject;
+
+namespace SpecStore.Wireup
+{
+	public static class Wireup
+	{
+		public static void ConfigureServices(this IServiceCollection services, IConfiguration configuration)
+		{
+
+		}
+
+		public static void ConfigureContainer(this IServiceRegistry registry)
+		{
+
+		}
+	}
+}
