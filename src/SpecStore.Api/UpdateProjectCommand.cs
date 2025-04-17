50b89a82e58d357ce961ab386a51f001ed9923f8
Mihucza, Bence <mihben@gmail.com>
2025-04-17 17:51:01 +0200

    fix: Made name parameter nnullable

--- src/SpecStore.Api/UpdateProjectCommand.cs
+++ src/SpecStore.Api/UpdateProjectCommand.cs
@@ -6,7 +6,7 @@ namespace SpecStore
 	public record UpdateProjectCommand : Command
 	{
 		public required string Key { get; init; }
-		public string Name { get; init; } = null!;
+		public string? Name { get; init; }
 	}
 }
 
